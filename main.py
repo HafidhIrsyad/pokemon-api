5c149c1941d4a53d9db4fbc27a7361769a363319
hafidh <hafidhkh97@gmail.com>
2025-02-25 22:22:46 +0700

    Initial commit for pokemon api

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    # Init database
+    
+    # Get Data Pokemon From Pokemon APi
+    
+    # Run App
+    app.run(debug=True)
