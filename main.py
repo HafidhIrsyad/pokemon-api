2987181b05d7810945d074b1dfc0753679734cc9
hafidh <hafidhkh97@gmail.com>
2025-02-25 22:27:18 +0700

    Init database connection

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import json
 from flask import Flask
+from database_conn import init_db
+
 
 app = Flask(__name__)
 
 if __name__ == "__main__":
     # Init database
+    init_db()
     
     # Get Data Pokemon From Pokemon APi
     
